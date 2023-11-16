4460674566b5ae7bc716b97b459984cdc23e820e
Agus-Sosa <sosaagustin351@gmail.com>
2023-11-16 17:11:34 -0300

    optimizando testing

--- test/carts-integration.test.js
+++ test/carts-integration.test.js
@@ -3,7 +3,7 @@ import supertest from 'supertest'
 import chai from 'chai'
 import { CartMongo } from '../src/dao/managers/mongo/CartMongo.js';
 import { UsersMongo } from '../src/dao/managers/mongo/UserMongo.js';
-import Cart from '../src/dao/models/cartModel.js';
+import e from 'express';
 
 const expect = chai.expect;
 const requester = supertest(app); /* Elemento para hacer peticiones al servidor */
@@ -29,10 +29,27 @@ describe("Pruebas app backend",async function(){
     describe("Pruebas modulo de carts", async function(){
         it('/GET /:cid', async function(){
 
-            const savedCart =await this.cartManager.createCart();
-            const cartId = savedCart._id;
+            const loginResponse = await requester.post('/api/sessions/login').send({
+                email: mockUser.email,
+                password: mockUser.password,
+            }).redirects(1);
+            const cookieResponse = loginResponse.headers['set-cookie'][0];
+            const cookieData = {
+                name: cookieResponse.split("=")[0],
+                value: cookieResponse.split("=")[1],
+            }
+            this.cookie = cookieData;
+            expect(loginResponse.status).to.equal(200);
 
-            const cartResponse = await requester.get(`/api/carts/${cartId}`);
+            const userData =await this.userManager.getByEmail(mockUser.email);
+            const userCart = userData.cart;
+            
+            // const savedCart =await this.cartManager.createCart();
+            // const cartId = savedCart._id;
+
+            const cartResponse = await requester
+            .get(`/api/carts/${userCart}`)
+            .set("Cookie", [`${this.cookie.name}=${this.cookie.value}`]);
             expect(cartResponse.status).to.equal(200);
 
         })
@@ -56,7 +73,7 @@ describe("Pruebas app backend",async function(){
                 price: 19.99,
                 thumbnails: 'https://ejemplo.com/imagen.jpg',
                 code: 'ABC123',
-                stock: 50,
+                stock: 100,
                 status: true,
                 category: 'gamer',
         
@@ -77,6 +94,9 @@ describe("Pruebas app backend",async function(){
 
             expect(cartResponse.status).to.equal(200);
         })
+
+
+
         it("/DELETE /:cid", async function(){
             const loginResponse = await requester.post('/api/sessions/login').send({
                 email: mockUser.email,
@@ -95,10 +115,149 @@ describe("Pruebas app backend",async function(){
 
             const response = await requester.delete(`/api/carts/${userCart}`);
             expect(response.status).to.equal(200);
+        })
+
+        it("/PUT /:cid/products/:pid", async function(){
+
+        
+        const loginResponse = await requester.post('/api/sessions/login').send({
+            email: mockUser.email,
+            password: mockUser.password,
+        }).redirects(1);
+        const cookieResponse = loginResponse.headers['set-cookie'][0];
+        const cookieData = {
+            name: cookieResponse.split("=")[0],
+            value: cookieResponse.split("=")[1],
+        }
+        this.cookie = cookieData;
+        expect(loginResponse.status).to.equal(200);
+        
+        const mockProduct = {
+            title: 'Producto de Ejemplo',
+            description: 'Este es un producto de muestra.',
+            price: 19.99,
+            thumbnails: 'https://ejemplo.com/imagen.jpg',
+            code: 'ABC123',
+            stock: 1000,
+            status: true,
+            category: 'gamer',
+    
+        };
+
+        const createProductResponse = await requester
+        .post('/api/products/createProduct')
+        .send(mockProduct)
+        .set("Cookie", [`${this.cookie.name}=${this.cookie.value}`]);
+    expect(createProductResponse.status).to.equal(200);
+        const productId = createProductResponse.body.product._id
+        const user = await this.userManager.getUserById(createProductResponse.body.product.owner)
+        const userCart = user.cart
+
+
+        const addToCartResponse = await requester
+            .post(`/api/carts/${userCart}/products/${productId}`)
+            .set("Cookie", [`${this.cookie.name}=${this.cookie.value}`]);
+            expect(addToCartResponse.status).to.equal(200);
+
+        const updateQuantity = 10;
+        const updateCartResponse = await requester
+            .put(`/api/carts/${userCart}/products/${productId}`)
+            .send({quantity: updateQuantity});
+        expect(updateCartResponse.status).to.equal(200);
+
+
+        })
+
+        it('/PUT /:cid', async function(){
+            
+        const loginResponse = await requester.post('/api/sessions/login').send({
+            email: mockUser.email,
+            password: mockUser.password,
+        }).redirects(1);
+        const cookieResponse = loginResponse.headers['set-cookie'][0];
+        const cookieData = {
+            name: cookieResponse.split("=")[0],
+            value: cookieResponse.split("=")[1],
+        }
+        this.cookie = cookieData;
+        expect(loginResponse.status).to.equal(200);
+        
+
+        const mockProduct = {
+            title: 'Producto de Ejemplo',
+            description: 'Este es un producto de muestra.',
+            price: 19.99,
+            thumbnails: 'https://ejemplo.com/imagen.jpg',
+            code: 'ABC123',
+            stock: 100,
+            status: true,
+            category: 'gamer',
+    
+        };
+
+    
+
+
+        const createProductResponse = await requester
+        .post('/api/products/createProduct')
+        .send(mockProduct)
+        .set("Cookie", [`${this.cookie.name}=${this.cookie.value}`]);
+    expect(createProductResponse.status).to.equal(200);
+
+        const productId = createProductResponse.body.product._id
+
+        const user = await this.userManager.getUserById(createProductResponse.body.product.owner)
+        const userCart = user.cart
+
+        const updateNewProductInCart = [
+            {
+                "product": productId,
+                "quantity": 60
+            }
+        ]
+
+        const updateCartReponse =await requester
+            .put(`/api/carts/${userCart}`)
+            .send(updateNewProductInCart)
+        
+        expect(updateCartReponse.status).to.equal(200);
+        })
+
+
+        it('/POST /:cid/purchase', async function(){
+           
+
+            const loginResponse = await requester.post('/api/sessions/login').send({
+                email: mockUser.email,
+                password: mockUser.password,
+            }).redirects(1);
+            const user = await this.userManager.getByEmail(mockUser.email);
+            const userCart = user.cart;
+
+            const cookieResponse = loginResponse.headers['set-cookie'][0];
+            const cookieData = {
+                name: cookieResponse.split("=")[0],
+                value: cookieResponse.split("=")[1],
+            }
+            this.cookie = cookieData;
+            expect(loginResponse.status).to.equal(200);
+
+            
+
+            const purchaseResponse = await requester
+                .post(`/api/carts/${userCart}/purchase`)
+                .set("Cookie", [`${this.cookie.name}=${this.cookie.value}`]);
+
+
+
+            expect(purchaseResponse.status).to.equal(200);
+
+        })
 
         })
 
         
-    })
+
+
 })
     
